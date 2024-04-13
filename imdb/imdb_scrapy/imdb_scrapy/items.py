1bdbc0c3963ca87d84ffafc2cfce659c20fefb59
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-04-13 14:09:06 +0330

    Added items commands.

--- imdb/imdb_scrapy/imdb_scrapy/items.py
+++ imdb/imdb_scrapy/imdb_scrapy/items.py
@@ -7,6 +7,14 @@ import scrapy
 
 
 class ImdbScrapyItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    movie_name = scrapy.Field()
+    movie_release = scrapy.Field()
+    movie_rating = scrapy.Field()
+    movie_vote = scrapy.Field()
+    movie_genre = scrapy.Field()
+    movie_duration = scrapy.Field()
+    movie_director = scrapy.Field()
+    movie_writer = scrapy.Field()
+    movie_stars = scrapy.Field()
+    movie_synopsis = scrapy.Field()
+    movie_link = scrapy.Field()
