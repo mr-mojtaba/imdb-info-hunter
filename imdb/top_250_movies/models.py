d0995b867eef5d1dbe20184558b06c99eb167b99
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-04-15 19:07:25 +0330

    Added movie model.

--- imdb/top_250_movies/models.py
+++ imdb/top_250_movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Movie(models.Model):
+    movie_id = models.AutoField(primary_key=True)
+    name = models.CharField(null=False, max_length=100)
+    release = models.IntegerField(null=False)
+    rating = models.FloatField(null=False)
+    duration = models.CharField(null=False, max_length=10)
+    synopsis = models.TextField(null=False)
+    link = models.URLField(null=False, max_length=100)
