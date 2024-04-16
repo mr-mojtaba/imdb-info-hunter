cd97b3ed446f56f658336912fb9aab3a817037fe
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-04-16 18:59:25 +0330

    Added all model classes.

--- imdb/top_250_movies/models.py
+++ imdb/top_250_movies/models.py
@@ -7,6 +7,48 @@ class Movie(models.Model):
     name = models.CharField(null=False, max_length=100)
     release = models.IntegerField(null=False)
     rating = models.FloatField(null=False)
+    vote = models.FloatField(null=False, max_length=10)
+    unit = models.CharField(null=True, max_length=1)
     duration = models.CharField(null=False, max_length=10)
     synopsis = models.TextField(null=False)
     link = models.URLField(null=False, max_length=100)
+
+
+class Genre(models.Model):
+    genre_id = models.AutoField(primary_key=True)
+    genre_name = models.CharField(null=False, max_length=20)
+
+
+class Director(models.Model):
+    director_id = models.AutoField(primary_key=True)
+    director_name = models.CharField(null=False, max_length=30)
+
+
+class Writer(models.Model):
+    writer_id = models.AutoField(primary_key=True)
+    writer_name = models.CharField(null=False, max_length=30)
+
+
+class Actor(models.Model):
+    actor_id = models.AutoField(primary_key=True)
+    actor_name = models.CharField(null=False, max_length=30)
+
+
+class MovieGenre(models.Model):
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+
+class MovieDirector(models.Model):
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    director_id = models.ForeignKey(Director, on_delete=models.CASCADE)
+
+
+class MovieWriter(models.Model):
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    Writer_id = models.ForeignKey(Writer, on_delete=models.CASCADE)
+
+
+class MovieActor(models.Model):
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    actor_id = models.ForeignKey(Actor, on_delete=models.CASCADE)
